d69895b0c91957e02342681b8677d97a026ed5ba
sokolov-ivan <sokolov.ivan.v@yandex.ru>
2021-12-25 16:39:43 +0300

    настраиваем поворот экрана в мэйн активити с помощью onSaveInstanceState(@NonNull Bundle outState)

--- app/src/main/java/com/example/android_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_calculator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android_calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,11 +13,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText numberField;
-    private Number number;
-
     private TextView result;
-//    private final static String keyNumber = "Number";
-
+    private String value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +23,36 @@ public class MainActivity extends AppCompatActivity {
         numberField = (EditText) findViewById(R.id.numberField);
         result = (TextView) findViewById(R.id.result);
         Number number = new Number(numberField.getText().toString());   //
-
-        findViewById(R.id.button_test).setOnClickListener(v -> {
-            String value = numberField.getText().toString();          //
+        if (savedInstanceState != null && savedInstanceState.containsKey("key")) {
+            value = savedInstanceState.getString("key");
+            result.setText(value);
             number.setVal(value);
-            result.setText(String.valueOf(number.getVal()));
+        }
+        findViewById(R.id.button_test).setOnClickListener(v -> {
+            value = numberField.getText().toString();          //
+            number.setVal(value);
+            result.setText(value);
         });
         findViewById(R.id.button_two_activity).setOnClickListener(v -> {
             Intent intent = new Intent(this, TwoActivity.class);
             intent.putExtra("Number", number);   //Number.class.getSimpleName()  - настояшие имя файла
             startActivity(intent);
         });
+
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key", value);
+    }
+
     //обработка нажатия на кнопку цифр и точки
     public void numberClick(View view) {
         Button button = (Button) view;
         numberField.append(button.getText());
     }
+
     //обработка нажатия на delete
     public void deleteClick(View view) {
         int len = numberField.getText().length(); //получаем длину
--- app/src/main/java/com/example/android_calculator/TwoActivity.java
+++ app/src/main/java/com/example/android_calculator/TwoActivity.java
@@ -24,5 +24,3 @@ public class TwoActivity extends AppCompatActivity {
 }
 
 
-
-
