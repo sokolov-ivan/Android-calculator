7aa8b92d6e0e7c39eaa7e84d2a54c67428946d9f
sokolov-ivan <sokolov.ivan.v@yandex.ru>
2021-12-28 14:49:37 +0300

    Правильно назвал переменные.Переделаны реакции на нажатия кнопок через OnClickListener.

--- app/src/main/java/com/example/android_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_calculator/MainActivity.java
@@ -10,11 +10,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
     private EditText numberField;
     private TextView result;
     private String value;
+
+    Button one_button;
+    Button two_button;
+    Button three_button;
+    Button four_button;
+    Button five_button;
+    Button six_button;
+    Button seven_button;
+    Button eight_button;
+    Button nine_button;
+    Button zero_button;
+    Button point_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,72 @@ public class MainActivity extends AppCompatActivity {
         numberField = (EditText) findViewById(R.id.numberField);
         result = (TextView) findViewById(R.id.result);
         Number number = new Number(numberField.getText().toString());   //
+
+        one_button = (Button) findViewById(R.id.one_button);
+        two_button = (Button) findViewById(R.id.two_button);
+        three_button = (Button) findViewById(R.id.three_button);
+        four_button = (Button) findViewById(R.id.four_button);
+        five_button = (Button) findViewById(R.id.five_button);
+        six_button = (Button) findViewById(R.id.six_button);
+        seven_button = (Button) findViewById(R.id.seven_button);
+        eight_button = (Button) findViewById(R.id.eight_button);
+        nine_button = (Button) findViewById(R.id.nine_button);
+        zero_button = (Button) findViewById(R.id.zero_button);
+        point_button = (Button) findViewById(R.id.point_button);
+
+        OnClickListener oMyButton = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.one_button:
+                        numberField.append(one_button.getText());
+                        break;
+                    case R.id.two_button:
+                        numberField.append(two_button.getText());
+                        break;
+                    case R.id.three_button:
+                        numberField.append(three_button.getText());
+                        break;
+                    case R.id.four_button:
+                        numberField.append(four_button.getText());
+                        break;
+                    case R.id.five_button:
+                        numberField.append(five_button.getText());
+                        break;
+                    case R.id.six_button:
+                        numberField.append(six_button.getText());
+                        break;
+                    case R.id.seven_button:
+                        numberField.append(seven_button.getText());
+                        break;
+                    case R.id.eight_button:
+                        numberField.append(eight_button.getText());
+                        break;
+                    case R.id.nine_button:
+                        numberField.append(nine_button.getText());
+                        break;
+                    case R.id.zero_button:
+                        numberField.append(zero_button.getText());
+                        break;
+                    case R.id.point_button:
+                        numberField.append(point_button.getText());
+                        break;
+
+                }
+            }
+        };
+        one_button.setOnClickListener(oMyButton);
+        two_button.setOnClickListener(oMyButton);
+        three_button.setOnClickListener(oMyButton);
+        four_button.setOnClickListener(oMyButton);
+        five_button.setOnClickListener(oMyButton);
+        six_button.setOnClickListener(oMyButton);
+        seven_button.setOnClickListener(oMyButton);
+        eight_button.setOnClickListener(oMyButton);
+        nine_button.setOnClickListener(oMyButton);
+        zero_button.setOnClickListener(oMyButton);
+        point_button.setOnClickListener(oMyButton);
+
         if (savedInstanceState != null && savedInstanceState.containsKey("key")) {
             value = savedInstanceState.getString("key");
             result.setText(value);
@@ -38,6 +118,13 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("Number", number);   //Number.class.getSimpleName()  - настояшие имя файла
             startActivity(intent);
         });
+        findViewById(R.id.delete_Click_button).setOnClickListener(v -> {
+            int len = numberField.getText().length(); //получаем длину
+            if (len > 0) {                                   //если без if то ошибка
+                numberField.getText().delete(len - 1, len);     //удаляем последний элемент
+            }
+        });
+
 
     }
 
@@ -47,23 +134,6 @@ public class MainActivity extends AppCompatActivity {
         outState.putString("key", value);
     }
 
-    //обработка нажатия на кнопку цифр и точки
-    public void numberClick(View view) {
-        Button button = (Button) view;
-        numberField.append(button.getText());
-    }
 
-    //обработка нажатия на delete
-    public void deleteClick(View view) {
-        int len = numberField.getText().length(); //получаем длину
-        if (len > 0) {                                   //если без if то ошибка
-            numberField.getText().delete(len - 1, len);     //удаляем последний элемент
-        }
-    }
 }
-
-
-
-
-
 
